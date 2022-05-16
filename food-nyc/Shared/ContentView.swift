393e717954341260d69aa9bb46c7327cb4026d60
David Massart <32443128+dadapoof@users.noreply.github.com>
2022-05-15 22:58:48 -0400

    First commit with buttons

---
+++ food-nyc/Shared/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by David Massart on 5/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: {}) {
+                Text("Restaurant")
+            }
+            Button(action: {}) {
+                Text("Bar")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ food-nyc/Shared/food_nycApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_nycApp.swift
+//  Shared
+//
+//  Created by David Massart on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct food_nycApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
